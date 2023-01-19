bf29a433ef764bc2d6859679177ccd034b0b2c57
PatrickB001 <patrickborgella1@gmail.com>
2023-01-19 13:56:20 -0600

    Finished full exercise

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
+const { Op } = require("sequelize");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const where = {};
+    if (req.query.tags) {
+      where.tags = {
+        [Op.like]: `%${req.query.tags}%`,
+      };
+    }
+    if (req.query.content) {
+      where.joke = {
+        [Op.like]: `%${req.query.content}%`,
+      };
+    }
+
+    const jokes = await Joke.findAll({ where });
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
